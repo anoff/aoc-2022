8719840869b40fc577b0c14476426d3a914bf0fa
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2022-12-02 22:34:57 +0900

    day2

---
+++ day02/solution.py
@@ -0,0 +1,64 @@
+def hand_score(sign: str) -> int:
+    if sign == "X": return 1
+    elif sign == "Y": return 2
+    elif sign == "Z": return 3
+    return 0
+
+def round_score(sign1: str, sign2: str) -> int:
+    if sign1 == "A":
+        if sign2 == "Y": return 6
+        elif sign2 == "Z": return 0
+        else: return 3
+    elif sign1 == "B":
+        if sign2 == "X": return 0
+        elif sign2 == "Z": return 6
+        else: return 3
+    elif sign1 == "C":
+        if sign2 == "X": return 6
+        elif sign2 == "Y": return 0
+        else: return 3
+    return 0
+
+def is_win(sign1: str, sign2: str) -> bool:
+    return round_score(sign1, sign2) == 6
+
+def get_hand(sign1: str, result: str) -> str:
+    if sign1 == "A":
+        if result == "Y": return "X"
+        elif result == "Z": return "Y"
+        else: return "Z"
+    elif sign1 == "B":
+        if result == "X": return "X"
+        elif result == "Z": return "Z"
+        else: return "Y"
+    elif sign1 == "C":
+        if result == "X": return "Y"
+        elif result == "Y": return "Z"
+        else: return "X"
+    return "-"
+
+def star1(input) -> None:
+    score = 0
+    for line in input:
+        sign1, sign2 = line.strip().split(" ")
+        score += round_score(sign1, sign2) + hand_score(sign2)
+    return score
+
+
+def star2(input) -> None:
+    score = 0
+    for line in input:
+        sign1, result = line.strip().split(" ")
+        sign2 = get_hand(sign1, result)
+        score += round_score(sign1, sign2) + hand_score(sign2)
+    return score
+
+def read_input(filepath: str) -> list[str]:
+    """Read input."""
+    input = list()
+    with open(filepath, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+if __name__ == "__main__":
+    print(f"Star1: {star1(read_input('input.txt'))}")
+    print(f"Star2: {star2(read_input('input.txt'))}")
---
+++ day02/solution_test.py
@@ -0,0 +1,34 @@
+from .solution import get_hand, hand_score, is_win, round_score, star1, star2
+
+def test_is_win():
+  assert is_win("A", "Y") == True
+  assert is_win("B", "X") == False
+  assert is_win("C", "Z") == False
+
+def test_round_score():
+  assert round_score("A", "Y") == 6
+  assert round_score("B", "X") == 0
+  assert round_score("C", "Z") == 3
+
+def test_hand_score():
+  assert hand_score("X") == 1
+  assert hand_score("Y") == 2
+  assert hand_score("Z") == 3
+
+def test_star1():
+  input = """A Y
+B X
+C Z""".split("\n")
+  assert star1(input) == 15
+
+def test_get_hand():
+  assert get_hand("A", "X") == "Z"
+  assert get_hand("A", "Y") == "X"
+  assert get_hand("B", "X") == "X"
+  assert get_hand("C", "Z") == "X"
+
+def test_star2():
+  input = """A Y
+B X
+C Z""".split("\n")
+  assert star2(input) == 12
