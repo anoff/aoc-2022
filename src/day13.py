6c8e22ded9777ab5a69a43bfac2c26b5261bc9af
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2022-12-14 11:28:55 +0000

    trial 13.2

--- src/day13.py
+++ src/day13.py
@@ -1,35 +1,43 @@
 from __future__ import annotations
 import json
 from typing import TypeAlias, Union
+import functools
 
 packet: TypeAlias = Union[int, list]
 
 
-def is_correct_order(p1: packet, p2: packet) -> Union[bool, None]:
+def is_correct_order(
+    p1: packet, p2: packet, is_nested: bool = False
+) -> Union[bool, None]:
     """Return true if two packets are in correct order.
 
-    Returns None if no decision can be made."""
+    Returns None if no decision can be made.
+
+    Args:
+        is_nested: set to True if this is a nested call."""
 
     if isinstance(p1, list) and isinstance(p2, list):
         for ix, one in enumerate(p1):
             if ix > len(p2) - 1:
                 return False
             two = p2[ix]
-            response = is_correct_order(one, two)
+            response = is_correct_order(one, two, is_nested=True)
             if isinstance(response, bool):
                 return response
-        return True
+        if not is_nested:
+            # only return true if this is outermost loop
+            return True
     elif isinstance(p1, int) and isinstance(p2, int):
         if p1 < p2:
             return True
         if p1 > p2:
             return False
     elif isinstance(p1, int) and isinstance(p2, list):
-        response = is_correct_order([p1], p2)
+        response = is_correct_order([p1], p2, is_nested=True)
         if isinstance(response, bool):
             return response
     elif isinstance(p1, list) and isinstance(p2, int):
-        response = is_correct_order(p1, [p2])
+        response = is_correct_order(p1, [p2], is_nested=True)
         if isinstance(response, bool):
             return response
 
@@ -44,9 +52,54 @@ def star1(lines: list[str]) -> int:
     return count
 
 
+def sort_packets(packets: list[packet]) -> list[packet]:
+    """Sort that from top to bottom the order is correct."""
+
+    # def compare(a: packet, b: packet) -> int:
+    #     result = is_correct_order(a, b)
+    #     if result is True:
+    #         return -1
+    #     if result is False:
+    #         return 1
+    #     return 0
+
+    # key_fn = functools.cmp_to_key(compare)
+
+    # sorted_packets = sorted(packets, key=key_fn)
+
+    cnt = 0
+    is_sorted = False
+    while not is_sorted and cnt < 100000:
+        cnt += 1
+        for ix, n in enumerate(packets[:-1]):
+            result = is_correct_order(packets[ix], packets[ix + 1])
+            if result is False:
+                put_away = packets.pop(ix)
+                packets.append(put_away)
+                break
+        if result != False:
+            is_sorted = True
+
+    return packets
+
+
 def star2(lines: list[str]) -> int:
     """Part2."""
-    return 0
+    packets = [[[2]], [[6]]]
+    for p in parse_input(lines):
+        packets.append(p[0])
+        packets.append(p[1])
+
+    s_list = sort_packets(packets)
+    divider = 1
+    for ix, p in enumerate(s_list):
+        if p == [[2]] or p == [[6]]:
+            divider *= ix + 1
+
+    return divider
+
+
+# 23165 too high
 
 
 def parse_input(text: list[str]) -> list[list[packet]]:
--- src/day13_test.py
+++ src/day13_test.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from .day13 import star1, star2, is_correct_order
+from .day13 import parse_input, sort_packets, star1, star2, is_correct_order
 
 TEST_INPUT = """[1,1,3,1,1]
 [1,1,5,1,1]
@@ -29,6 +29,20 @@ TEST_INPUT = """[1,1,3,1,1]
 
 
 def test_is_correct_order() -> None:
+    assert (
+        is_correct_order(
+            [[6]],
+            [
+                [[], 2, 9, 4],
+                [],
+                [[[], [0]], [[7, 0, 10, 8], [10, 5, 5, 4, 1], 0, 6]],
+                [],
+            ],
+        )
+        == False
+    )
+    assert is_correct_order([[4, 4], 4, 4, 4], [[4, 4], 4, 4]) == False
+    assert is_correct_order([[4, 4], 4, 4], [[4, 4], 4, 4, 4]) == True
     assert is_correct_order([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) == True
     assert is_correct_order([[1], [2, 3, 4]], [[1], 4]) == True
     assert is_correct_order([9], [[8, 7, 6]]) == False
@@ -44,5 +58,18 @@ def test_star1() -> None:
     assert star1(TEST_INPUT) == 13
 
 
+def test_sort_packets() -> None:
+    packets = [[[2]], [[6]]]
+    for p in parse_input(TEST_INPUT):
+        packets.append(p[0])
+        packets.append(p[1])
+
+    s_list = sort_packets(packets)
+    assert s_list[0] == []
+    assert s_list[3] == [1, 1, 3, 1, 1]
+    assert s_list[7] == [1, [2, [3, [4, [5, 6, 7]]]], 8, 9]
+    assert s_list[9] == [[2]]
+
+
 def test_star2() -> None:
-    assert star2(TEST_INPUT) == 0
+    assert star2(TEST_INPUT) == 140
